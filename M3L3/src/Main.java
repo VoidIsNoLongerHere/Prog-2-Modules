a35b5d9d1ab8b64e8f895ee360cdc34fd11544c2
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-12 15:27:20 -0600

    Update Main.java

--- M3L3/src/Main.java
+++ M3L3/src/Main.java
@@ -1,8 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         // Init
-        IOSimplfier i = new IOSimplfier();
+        IOSimplfier i = new IOSimplfier(); // I did make this, essentially is just input func from python and uses scanner as the base
+
+        // Create all the classes with input from user
+        Course course = new Course(
+                i.inStr("Please enter the Course name: "),
+                i.inStr( "Please enter the course code: ")
+        );
+
+        Professor professor = new Professor(i.inStr("Please enter the professor's name: "),
+                i.inStr("Please enter the department: "),
+                course //Course Class
+        );
+
+        University university = new University(
+                i.inStr("Please enter the university's name: "),
+                i.inStr("Please enter the university's location: "),
+                professor // Professor Class
+        );
+
 
         
+
     }
 }
