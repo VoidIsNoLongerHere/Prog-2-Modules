d61221b76e7cdeb989274dd514db899d7695f315
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-12 15:09:48 -0600

    Create University.java

---
+++ M3L3/src/University.java
@@ -0,0 +1,39 @@
+public class University {
+    // var
+    String name, location;
+    Professor professor;
+
+    // Constructor
+
+    public University(String name, String location, Professor professor) {
+        this.name = name;
+        this.location = location;
+        this.professor = professor;
+    }
+
+    // Get/Set
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+}
