424d81ad6d909a7e683a309778761cd72df527ab
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-12 15:05:11 -0600

    M3L3 Course Class

---
+++ M3L3/src/Course.java
@@ -0,0 +1,28 @@
+public class Course {
+    //Var
+    String name, code;
+
+    //Constructor
+    public Course(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    // Get/Set
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
