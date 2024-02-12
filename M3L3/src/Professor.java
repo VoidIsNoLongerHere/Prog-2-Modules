86178f5e0a56059cdbca4c8d4bd05bc2e6cd79e5
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-12 15:07:25 -0600

    Update Professor.java

--- M3L3/src/Professor.java
+++ M3L3/src/Professor.java
@@ -1,3 +1,37 @@
 public class Professor {
-    
+    String name, department;
+    Course course;
+
+    //Constructor
+    public Professor(String name, String department, Course course) {
+        this.name = name;
+        this.department = department;
+        this.course = course;
+    }
+
+    // Get/Set
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
