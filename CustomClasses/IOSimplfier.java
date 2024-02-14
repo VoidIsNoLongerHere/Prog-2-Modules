0c5d83564e30f6276fcebad6218612bec44d2495
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-13 18:58:24 -0600

    Update IOSimplfier.java
    Added Disclaimer

--- CustomClasses/IOSimplfier.java
+++ CustomClasses/IOSimplfier.java
@@ -1,5 +1,24 @@
+/*
+ * This is a custom class written by:
+ * Landon Norman
+ *
+ * You are allowed to use this class as an example or inspiration for your own
+ * You are NOT allowed to claim this code as your own unless there is a substantial change or written permission from the author
+ *
+ * This comment, github commits, and other snippets of code will be used as evidence in the case of academic dishonesty
+ */
+
 import java.util.Scanner;
 
+
+/*
+* This entire class is equal to
+*
+* System.out.print(prompt);
+  return s.nextLine();
+
+* Major changes not needed as its just that
+*/
 public class IOSimplfier {
     Scanner s = new Scanner(System.in);
     
