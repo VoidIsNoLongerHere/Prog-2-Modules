6f652d618182536a05c6ceabea26856f6ea302b3
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-20 19:32:04 -0600

    Create Tree.java
    Interface for tree class for outputting

---
+++ IOTest/src/Tree.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public interface Tree {
+    public ArrayList<Objects> outputData();
+    public int getDepth();
+    public void setDepth();
+
+}
