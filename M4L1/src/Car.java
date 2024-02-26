7d651bac5733bae083a1a4306ca6cca932a7df38
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-26 17:29:32 -0600

    m$l4

---
+++ M4L1/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String make;
+    String model;
+    int year;
+    double price;
+
+    public Car(String make, String model, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+}
---
+++ M4L1/src/Main.java
@@ -0,0 +1,14 @@
+
+// Landon Norman
+// Module 4 Lab 1
+// 2/26/2024
+// Purpose: Display arraylist of car objects
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Car> carLot = new ArrayList<Car>(5);
+    }
+}
