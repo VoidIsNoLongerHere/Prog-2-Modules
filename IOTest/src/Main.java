c3bd241c395e6dbbe3729118302e6d4922c2e983
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-27 19:20:26 -0600

    Update Main.java

--- IOTest/src/Main.java
+++ IOTest/src/Main.java
@@ -2,11 +2,11 @@ import com.sun.jdi.InvalidTypeException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(InGen.in("Enter a string: ", String.class) instanceof String);
-        System.out.println(InGen.in("Enter a Integer: ", Integer.class) instanceof Integer);
-        System.out.println(InGen.in("Enter a Float: ", Float.class) instanceof Float);
-        System.out.println(InGen.in("Enter a Double: ", Double.class) instanceof Double);
-        System.out.println(InGen.in("Enter a Boolean: ", Boolean.class) instanceof Boolean);
+        System.out.println(InGen.in("Enter a string: ", String.class));
+        System.out.println(InGen.in("Enter a Integer: ", Integer.class));
+        System.out.println(InGen.in("Enter a Float: ", Float.class));
+        System.out.println(InGen.in("Enter a Double: ", Double.class));
+        System.out.println(InGen.in("Enter a Boolean: ", Boolean.class));
 
 
 
