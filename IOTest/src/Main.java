7fb66231018a7190a50b2871d28d83e6eaec63aa
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-27 19:16:11 -0600

    Update Main.java

--- IOTest/src/Main.java
+++ IOTest/src/Main.java
@@ -1,7 +1,7 @@
 import com.sun.jdi.InvalidTypeException;
 
 public class Main {
-    public static void main(String[] args) throws InvalidTypeException {
+    public static void main(String[] args) throws Exception {
         System.out.println(InGen.in("Enter a string: ", String.class) instanceof String);
         System.out.println(InGen.in("Enter a Integer: ", Integer.class) instanceof Integer);
         System.out.println(InGen.in("Enter a Float: ", Float.class) instanceof Float);
