477382c0924b5db3728d6eb82146898627d976b5
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-02-29 15:14:04 -0600

    Update Main.java
    Driver for lab

--- m5l1/src/Main.java
+++ m5l1/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Ship cargo = new CargoShip(
+                InGen.in("Please enter the name of the cargo ship: ", String.class),
+                InGen.in("Please enter the year it was built: ", Integer.class),
+                InGen.in("Please enter the weight in tons: ", Integer.class)
+                );
+
+        Ship cruise = new CruiseShip(
+                InGen.in("Please enter the name of the cruise ship: ", String.class),
+                InGen.in("Please enter the year it was built: ", Integer.class),
+                InGen.in("Please enter the maximum about of passengers: ", Integer.class)
+        );
+
+        System.out.println(cargo.toString());
+        System.out.println(cruise.toString());
     }
 }
