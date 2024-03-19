7adb212f15e7bff471b31babc15c30d9878b1478
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-03-19 18:52:22 -0500

    IOTest

--- IOTest/src/Leaflet.java
+++ IOTest/src/Leaflet.java
@@ -2,12 +2,34 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public interface Leaflet  {
-    void printSelf(int depth); // Required to printchild as well if able
+
+    /**
+     * Print data from self, then print children recursively. Depth is the amount of indentation.
+     *
+     * @param depth Tabs before the data printed. Should ideally never be set manually
+     */
+    void printSelf(int depth);
+
+    /**
+     * Generates a string of "\t"
+     * @param depth Integer amount of tabs
+     * @return A string of "\t" characters
+     */
     default String tabs(int depth){
-        return "\t".repeat(depth);
+        if (depth > 0){return "\t".repeat(depth);}
+        return "";
     }
+
+    /**
+     * Adds a Leaflet object to the child list attribute
+     * @param child Implemented Leaflet object
+     */
     void addChild(Leaflet child);
 
+    /**
+     * Print children from self, this should never be called manually
+     * @param depth Tabs before data is printed,
+     */
     void printChildren(int depth);
 
 }
