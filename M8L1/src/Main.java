be73ca70f925a93c160641e7e6c1c8abb001d68d
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-04-18 18:38:37 -0500

    Change to use ReportWriter

--- M8L1/src/Main.java
+++ M8L1/src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ReportHeader reportHeader = ReportHeader.getInstance();
-        ReportFooter reportFooter = ReportFooter.getInstance();
-        reportHeader.printHeader(); // Using the new printHeader method
-
-        // Write report
-
-        reportFooter.printFooter();
+        ReportWriter report = new ReportWriter();
+        report.printReport("Im writing things Yay!!!!!");
     }
 }
