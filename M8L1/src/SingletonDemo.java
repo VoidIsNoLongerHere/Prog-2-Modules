57b7bce4a0a9e7a882f24e8a5602ecedbd16896f
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-04-18 18:17:14 -0500

    Remove SingletonDemo

--- M8L1/src/SingletonDemo.java
+++
@@ -1,8 +0,0 @@
-public class SingletonDemo {
-    public static void main(String[] args) {
-        ReportHeader reportHeader = ReportHeader.getInstance();
-        reportHeader.printHeader(); // Using the new printHeader method
-
-        // Write report
-    }
-}
