ae03afef3a46147fec50d9fb0b5a75b616a0ccf6
VoidIsNoLongerHere <theninjaracoon@gmail.com>
2024-04-18 19:00:01 -0500

    Add Initial Code

---
+++ M8L3/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        WeatherStation weatherStation = new WeatherStation();
+        Observer weatherDisplay = new WeatherDisplay();
+        Observer mobileApp = new MobileApp();
+
+        weatherStation.addObserver(weatherDisplay);
+        weatherStation.addObserver(mobileApp);
+
+        // Simulate temperature change
+        weatherStation.setTemperature(25.0f);
+        System.out.println();
+        // Simulate temperature change again
+        weatherStation.setTemperature(30.0f);
+        System.out.println();
+
+        // Unregister an observer
+        weatherStation.setTemperature(33.0f);
+        weatherStation.removeObserver(mobileApp);
+        System.out.println();
+        System.out.println("Now we have removed the Mobile App Observer");
+        weatherStation.setTemperature(33.0f);
+    }
+}
